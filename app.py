2faaee2426f8b3286b49eb77e98a0d5541861ff7
jdturbeau <turbeaucroteau@gmail.com>
2025-09-15 22:41:58 -0400

    Update app.py

--- app.py
+++ app.py
@@ -9,18 +9,37 @@ app = Flask(__name__)
 @app.route("/")
 def index():
    #return render_template("index.html")
-  strWebOutput = "<head>"
-  strWebOutput += "<title>TechSushi - Portfolio</title>"
-  strWebOutput += "</head>"
-  strWebOutput += "<body>Welcome to the Tech Sushi - Portfolio page<br><br><br>"
-  strWebOutput += "Would you like to visit:<br><br>"
-  strWebOutput += "<a href=\"/keysset\">KV set</a><br><br>"
-  strWebOutput += "<a href=\"/keysget\">KV retrieve</a><br><br>"
-  strWebOutput += "<a href=\"/demo\">demo second page routing</a><br><br>"
-  strWebOutput += "<a href=\"/refreshtoken\">refresh api token</a><br><br>"
-  strWebOutput += "<a href=\"/getcontent\">use api token to get content</a><br><br>"
-  strWebOutput += "<a href=\"/testpost\">test posting value</a><br><br><br>"
-  strWebOutput += "</form><br><br><br><br>"
-  strWebOutput += "Run through version [060]</body>"
+   strWebOutput = "<head>"
+   strWebOutput += "<title>TechSushi - Portfolio</title>"
+   strWebOutput += "</head>"
+   strWebOutput += "<body>Welcome to the TechSushi - Portfolio page<br><br><br>"
+   strWebOutput += "Would you like to visit:<br><br>"
+
+   strWebOutput += "<a href=\"/redmedia\">Reddit media retreiver</a><br><br>"
+   
+   strWebOutput += "<a href=\"/keysset\">KV set</a><br><br>"
+   strWebOutput += "<a href=\"/keysget\">KV retrieve</a><br><br>"
+   strWebOutput += "<a href=\"/demo\">demo second page routing</a><br><br>"
+   strWebOutput += "<a href=\"/refreshtoken\">refresh api token</a><br><br>"
+   strWebOutput += "<a href=\"/getcontent\">use api token to get content</a><br><br>"
+   strWebOutput += "<a href=\"/testpost\">test posting value</a><br><br><br>"
+   strWebOutput += "</form><br><br><br><br>"
+   strWebOutput += "Run through version [060]</body>"
   
-  return strWebOutput
+   return strWebOutput
+
+@app.route("/redmedia")
+def redmedia():
+   #table with
+   #   overview, what, technologies involved,
+   strWebOutput = "<head>"
+   strWebOutput += "<title>TechSushi - Portfolio</title>"
+   strWebOutput += "</head>"
+   strWebOutput += "<body>Welcome to the TechSushi - Portfolio page<br><br><br>"
+   strWebOutput += "<br><br><br><br>"
+   strWebOutput += "Run through version [060]</body>"
+  
+   return
+
+if __name__ == '__main__':
+   app.run(debug=True)
