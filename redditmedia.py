dd7da1b44c2ee010cd62a6b8c299d58d41373590
jdturbeau <turbeaucroteau@gmail.com>
2025-09-17 01:02:29 -0400

    Update redditmedia.py

--- redditmedia.py
+++ redditmedia.py
@@ -190,8 +190,8 @@ def reddit_jsontohtml(jsonContent, strDestURL):
 def html_form(strDestination):
    
    strWebOutput = f"<form action=\"/{strDestination}\" method=\"post\"><!-- Form elements go here -->"
-   strWebOutput += f"<label for=\"name\">Subreddit:</label><br><input type=\"text\" id=\"subreddit\" name=\"sub\" placeholder=\"p320\" autocomplete=\"off\"><br>"
-   strWebOutput += f"<input type=\"checkbox\" id=\"pictures\" name=\"mediatype\" value=\"pictures\" checked><label for=\"pictures\">Pictures</label>"
+   strWebOutput += f"<label for=\"name\">Subreddit:</label><br><input type=\"text\" id=\"subreddit\" name=\"sub\" placeholder=\"p320\" autocomplete=\"off\"><br><br>"
+   strWebOutput += f"<label for=\"mediatype\">Type of Media:</label><br><input type=\"checkbox\" id=\"pictures\" name=\"mediatype\" value=\"pictures\" checked><label for=\"pictures\">Pictures</label>"
    strWebOutput += f"<input type=\"checkbox\" id=\"videos\" name=\"mediatype\" value=\"videos\"><label for=\"videos\">Videos</label><br><br>"
    #add new, hot, rising, controversial, top
    strWebOutput += f"<label for=\"sort\">Choose Sort Order:</label><br>"
