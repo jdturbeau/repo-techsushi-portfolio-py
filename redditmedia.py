d9a72ed91cf05c604131f29e152b1297f3db6f3a
JD Turbeau <turbeaucroteau@gmail.com>
2025-09-21 23:06:08 -0400

    Update redditmedia.py

--- redditmedia.py
+++ redditmedia.py
@@ -229,7 +229,7 @@ def html_form(strDestination):
    strFormOutput = f"<form action=\"/{strDestination}\" method=\"post\"><!-- Form elements go here -->"
    strFormOutput += f"<label for=\"name\">Subreddit:</label><input type=\"text\" id=\"subreddit\" name=\"sub\" placeholder=\"all\" autocomplete=\"off\">"
    strFormOutput += f"<input type=\"checkbox\" id=\"images\" name=\"mediatype\" value=\"images\" checked><label for=\"images\">Images</label>"
-   strFormOutput += f"<input type=\"checkbox\" id=\"videos\" name=\"mediatype\" value=\"videos\" checked><label for=\"videos\">Videos</label>
+   strFormOutput += f"<input type=\"checkbox\" id=\"videos\" name=\"mediatype\" value=\"videos\" checked><label for=\"videos\">Videos</label>"
    strFormOutput += f"<br><br>"
    strFormOutput += f"<label for=\"sort\">Sort by:</label><select id=\"sort\" name=\"sort\">"
    strFormOutput += f"<option value=\"new\" selected=\"true\">New</option>"
@@ -243,7 +243,7 @@ def html_form(strDestination):
    strFormOutput += f"<br><br>"
    strFormOutput += f"<input type=\"radio\" id=\"list\" name=\"view\" value=\"list\" checked><label for=\"list\">List View</label>"
    strFormOutput += f"<input type=\"radio\" id=\"gallery\" name=\"view\" value=\"gallery\"><label for=\"gallery\">Gallery View</label>"
-   strFormOutput += f"<input type=\"checkbox\" id=\"nsfw\" name=\"nsfw\" value=\"nsfw\" disabled><label for=\"nsfw\">Allow over_18 flag?</label>
+   strFormOutput += f"<input type=\"checkbox\" id=\"nsfw\" name=\"nsfw\" value=\"nsfw\" disabled><label for=\"nsfw\">Allow over_18 flag?</label>"
    strFormOutput += f"<br><br>"
    strFormOutput += f"<button type=\"submit\">Browse Media</button></form><br><br>"
    #add (media by) username
