7b8578d7b54e9e88c5c3fef9b20e53a8542d56ce
JD Turbeau <turbeaucroteau@gmail.com>
2025-09-21 23:55:14 -0400

    Create overview.py

---
+++ overview.py
@@ -0,0 +1,46 @@
+'''
+Project Name: TechSushi
+Purpose:
+Secondary Purpose:
+In Scope:
+Out of Scope:
+Technology and/or skills demonstrated: 
+(road map?) Feature Improvements:
+Security Improvements:
+
+
+
+'''
+
+'''
+Project Name: RedMed (Reddit Media Viewer)
+Purpose:
+Secondary Purpose:
+In Scope:
+Out of Scope:
+Technology and/or skills demonstrated: Azure webapp, Azure keyvault, GitHub, CI/CD, Python, API usage
+(road map?) Feature Improvements:
+  results returned condition
+  gallery view
+  over_18 toggle
+  images and/or videos filter
+  next link vs refresh link
+Security Improvements: Private Network Endpoints (cost)
+
+
+
+'''
+
+'''
+Project Name: LinkedIn Search
+Purpose:
+Secondary Purpose:
+In Scope:
+Out of Scope:
+Technology and/or skills demonstrated: 
+(road map?) Feature Improvements:
+Security Improvements:
+
+
+
+'''
