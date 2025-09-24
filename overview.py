ca1f70f2bbdfb9cb1f4120bbdce83fda77160300
JD Turbeau <turbeaucroteau@gmail.com>
2025-09-24 11:40:24 -0400

    Update overview.py

--- overview.py
+++ overview.py
@@ -1,34 +1,71 @@
 '''
-Project Name: TechSushi
+Project Name: (template)
 Purpose:
 Secondary Purpose:
 In Scope:
 Out of Scope:
+Technology and/or skills demonstrated:
+(road map?) Feature Improvements:
+Security Improvements: 
+'''
+
+'''
+Project Name: TechSushi
+Purpose:
+  Create a FREE website portfolio demonstrating knowledge of
+    Infrastructure as a Service/Platform as a Service (Azure, AWS)
+    Python
+Secondary Purpose:
+  Platform to share articles and blog opinions
+In Scope:
+Out of Scope:
+  Yearly Costs - Domain name and DNS
 Technology and/or skills demonstrated: 
 (road map?) Feature Improvements:
+  Resume section
+  Blog utilizing MarkDown with three example articles
+  Portfolio - Redundancy / high availability with multiple cloud vendors
+  Portfolio - Honeypot with detection and auto mitigate with alert for verification
+  Portfolio - Social Forum Media Scraper
+  Portfolio - OIDC OpenID example
+  Portfolio - Status / DNS check / Content Change Alerting
+  SEO
+  About Me Video
 Security Improvements:
-
-
-
+  Private Network Endpoints (cost)
+  Reduce single points of failure
+  Cost restrictions and monitoring (to prevent malicious overages)
 '''
 
 '''
 Project Name: RedMed (Reddit Media Viewer)
 Purpose:
+  Retrieve media (photos and videos) from Reddit and display in a river, or infinite scroll, format style
 Secondary Purpose:
+  [Real World Use] - Identify potential bots/catfish by tracking duplicate images between different accounts and subreddits
 In Scope:
 Out of Scope:
-Technology and/or skills demonstrated: Azure webapp, Azure keyvault, GitHub, CI/CD, Python, API usage
+Technology and/or skills demonstrated: Azure webapp, Azure keyvault, GitHub, Azure DevOps, CI/CD, Python, API usage
 (road map?) Feature Improvements:
   results returned condition
   gallery view
   over_18 toggle
   images and/or videos filter
   next link vs refresh link
-Security Improvements: Private Network Endpoints (cost)
+Security Improvements:
+  Private Network Endpoints (cost)
+  Input validation / OWASP
+'''
 
-
-
+'''
+Project Name: OIDC Entra integration
+Purpose:
+Secondary Purpose:
+In Scope:
+Out of Scope:
+Technology and/or skills demonstrated:
+(road map?) Feature Improvements:
+Security Improvements: 
 '''
 
 '''
@@ -40,7 +77,4 @@ Out of Scope:
 Technology and/or skills demonstrated: 
 (road map?) Feature Improvements:
 Security Improvements:
-
-
-
 '''
