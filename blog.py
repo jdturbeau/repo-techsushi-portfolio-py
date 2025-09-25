362d03d71b740de8ce9c227c1e5e36b7a1d7f607
JD Turbeau <turbeaucroteau@gmail.com>
2025-09-25 12:11:45 -0400

    Update blog.py

--- blog.py
+++ blog.py
@@ -1,16 +1,16 @@
 #from flask import (Flask, redirect, render_template, request, send_from_directory, url_for)
 #import requests
 #import os
-import markdown
+#import markdown
 
 def blog_post(strPostFile):
   
   with open(f"_posts/{strFile}", "r") as p:
     strContent = p.read()
-  strHTML = markdown.markdown(strContent)
+  #strHTML = markdown.markdown(strContent)
 
-  #alt: return render_template("index.html", post_content=strHTML)
-  return strHTML
+  ##alt: return render_template("index.html", post_content=strHTML)
+  return strContent
 
 def blog_recent(intCount):
   return
