134ebf94a3f9c4ec513d6b14d65cad41b17494a0
JD Turbeau <turbeaucroteau@gmail.com>
2025-09-25 19:08:11 -0400

    Update blog.py

--- blog.py
+++ blog.py
@@ -5,21 +5,31 @@ import markdown
 
 def blog_post(strPostFile):
   
-  #check if file string is not null, then check if file exists
-  with open(f"_posts/{strPostFile}", "r") as p:
-    strContent = p.read()
-  #strHTML = markdown.markdown(strContent)
-
+  try:
+    #check if file string is not null, then check if file exists
+    with open(f"./_posts/{strPostFile}", "r") as p:
+      strContent = p.read()
+    #strHTML = markdown.markdown(strContent)
+  except Exception as e:
+      #could contain sensitive information in error message
+      strSetOutput = f"an unexpected error occurred during <b>BLOG GET</b>: {e}<br><br>"
+      return strSetOutput
   ##alt: return render_template("index.html", post_content=strHTML)
+
   return strContent
 
 def blog_recent(intCount):
 
-  if not intCount:
-    intCount = 5
-    
-  os.chdir("_posts/")
-  lstFiles = filter(os.path.isfile, os.listdir("_posts/"))
-  lstFiles.sort(key=os.path.getmtime)
+  try:
+    if not intCount:
+      intCount = 5
+      
+    os.chdir("./_posts/")
+    lstFiles = filter(os.path.isfile, os.listdir("./_posts/"))
+    lstFiles.sort(key=os.path.getmtime)
+  except Exception as e:
+      #could contain sensitive information in error message
+      strSetOutput = f"an unexpected error occurred during <b>BLOG RECENT</b>: {e}<br><br>"
+      return strSetOutput
   
   return lstFiles
