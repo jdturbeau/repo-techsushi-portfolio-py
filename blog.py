c74ec487753281c240607c4cfa2ba42c21e8a8f2
JD Turbeau <turbeaucroteau@gmail.com>
2025-09-24 21:09:33 -0400

    Create blog.py

---
+++ blog.py
@@ -0,0 +1,8 @@
+#from flask import (Flask, redirect, render_template, request, send_from_directory, url_for)
+#import requests
+
+def blog_post(strLabel):
+  return
+
+def blog_recent(intCount):
+  return
