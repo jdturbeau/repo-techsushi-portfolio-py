863a83e8cfde0ddc5053c873eb4fbe6851347bd2
JD Turbeau <turbeaucroteau@gmail.com>
2025-09-25 17:01:52 -0400

    Update blog.py

--- blog.py
+++ blog.py
@@ -5,6 +5,7 @@
 
 def blog_post(strPostFile):
   
+  #check if file string is not null, then check if file exists
   with open(f"_posts/{strPostFile}", "r") as p:
     strContent = p.read()
   #strHTML = markdown.markdown(strContent)
@@ -13,4 +14,12 @@ def blog_post(strPostFile):
   return strContent
 
 def blog_recent(intCount):
-  return
+
+  if not intCount:
+    intCount = 5
+    
+  os.chdir("_posts/")
+  lstFiles = filter(os.path.isfile, os.listdir("_posts/"))
+  lstFiles.sort(key=os.path.getmtime)
+  
+  return lstFiles
