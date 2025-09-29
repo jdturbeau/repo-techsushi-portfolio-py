154a48b96f9ea2a2b87ab14646b56245a5017949
JD Turbeau <turbeaucroteau@gmail.com>
2025-09-29 13:51:20 -0400

    Update blog.py

--- blog.py
+++ blog.py
@@ -2,24 +2,80 @@
 #import requests
 import os #needed for chdir and getcwd
 import markdown
-import re
+#import re
 
-def blog_post(strPostFile):
+def blog_parsefile(strParseFile):
+
+  #input:    post full path filename, typically .md
+  #output:   dictionary with named attributes from file
   
-  '''
   try:
-    #check if file string is not null, then check if file exists
-    with open(f"_posts/{strPostFile}", "r") as p:
-      strContent = p.read()
-    #strHTML = markdown.markdown(strContent)
+    
+    #check if file string is not null
+    #  then check if file exists
+    
+    #consider "_posts/" if needed
+    with open(f"{strParseFile}", "r") as p:
+      strParseContent = p.read()
+    
   except Exception as e:
       #could contain sensitive information in error message
-      strSetOutput = f"an unexpected error occurred during <b>BLOG GET</b>: {e}<br><br>"
+      strSetOutput = f"an unexpected error occurred during <b>BLOG PARSE FILE read</b>: {e}<br><br>"
       return strSetOutput
   ##alt: return render_template("index.html", post_content=strHTML)
-  '''
-  return #strContent
+  
+  try:
+    strPattern = r"(?<=title: ).*"   #gi - use re.ignorecase below
+    strTitle = re.search(strPattern, strParseContent, re.IGNORECASE
+    strPattern = r"(?<=date: ).*"
+    strDate = re.search(strPattern, strParseContent, re.IGNORECASE)
+    strPattern = r"(?<=author: ).*"
+    strAuthor = re.search(strPattern, strParseContent, re.IGNORECASE)
+    #future
+    #strPattern = r"(?<=tags: ).*"
+    #strTags = re.search(strPattern, strParseContent, re.IGNORECASE)
+    strPattern = r"(?<=-----\n)[\s\S]*"
+    strBody = re.search(strPattern, strParseContent, re.IGNORECASE)
+  
+    dictParsed = {"File": strParseFile, "Title": strTitle, "Date": strDate, "Author": strAuthor, "Body": strBody}
+  except Exception as e:
+      #could contain sensitive information in error message
+      strSetOutput = f"an unexpected error occurred during <b>BLOG PARSE FILE dictionary creation</b>: {e}<br><br>"
+      return strSetOutput
 
+  #caller can check if output is dictionary or string(error)
+  #  if isinstance(var, dict):  #checks subclasses of dict
+  #  if type(var) is dict:      #does not
+  #  can replace dict with str
+
+  return dictParsed
+
+def blog_formatpost(dictPostAttribs):
+  
+  try:
+    #verify dictBriefAttribs first
+    if not isinstance(dictPostAttribs, dict):
+      strTypeOutput = type(dictPostAttribs)
+      strSetOutput = f"an unexpected error occurred during <b>BLOG FORMAT POST: dictionary variable invalid type: [ {strTypeOutput} ]</b><br><br>"
+      return strSetOutput
+    
+    #File for filename, may need to trim, used for crafting link
+    
+  except Exception as e:
+    #could contain sensitive information in error message
+    strSetOutput = f"an unexpected error occurred during <b>BLOG PARSE FILE dictionary creation</b>: {e}<br><br>"
+    return strSetOutput
+  
+    #strHTML = markdown.markdown(strContent)
+  return
+
+def blog_formatbrief(dictBriefAttribs):
+
+  #verify dictBriefAttribs first
+  #File for filename, may need to trim, used for crafting link
+  
+  return
+  
 def blog_recent(intCount):
 
   try:
@@ -43,8 +99,8 @@ def blog_recent(intCount):
     
     for strPostFile in lstSortedFiles:
       #parse file results for article filename, title, date, author, (tags?), body, and formating
-      lstBlogAttrib = blog_postparse(strPostFile)
-      strBriefFormat = blog_briefformat(lstBlogAttrib)
+      dictBlogAttrib = blog_parsefile(strPostFile)
+      strBriefFormat = blog_formatbrief(dictBlogAttrib)
       strSetOutput += strBriefFormat
     
   except Exception as e:
@@ -53,83 +109,3 @@ def blog_recent(intCount):
       return strSetOutput
   
   return strSetOutput
-
-def blog_postparse(strParseFile):
-
-  try:
-    #check if file string is not null
-    #  then check if file exists
-    with open(f"_posts/{strParseFile}", "r") as p:
-      strParseContent = p.read()
-    
-  except Exception as e:
-      #could contain sensitive information in error message
-      strSetOutput = f"an unexpected error occurred during <b>BLOG GET</b>: {e}<br><br>"
-      return strSetOutput
-  ##alt: return render_template("index.html", post_content=strHTML)
-  
-  strPattern = r"(?<=title: ).*"   #gi - use re.ignorecase below
-  strTitle = re.search(strPattern, strParseContent, re.IGNORECASE
-  strPattern = r"(?<=date: ).*"   #gi - use re.ignorecase below
-  strDate = re.search(strPattern, strParseContent, re.IGNORECASE)
-  strPattern = r"(?<=author: ).*"   #gi - use re.ignorecase below
-  strAuthor = re.search(strPattern, strParseContent, re.IGNORECASE)
-  #future
-  #strPattern = r"(?<=tags: ).*"   #gi - use re.ignorecase below
-  #strTags = re.search(strPattern, strParseContent, re.IGNORECASE)
-  strPattern = r"(?<=-----\n)[\s\S]*"   #gi - use re.ignorecase below
-  strBody = re.search(strPattern, strParseContent, re.IGNORECASE)
-
-  dictParsed = {"File": strParseFile, "Title": strTitle, "Date": strDate, "Author": strAuthor, "Body": strBody}
-  return dictParsed
-
-def blog_postformat(dictPostAttribs):
-  #strHTML = markdown.markdown(strContent)
-  return
-
-def blog_briefformat(dictBriefAttribs):
-
-  #verify dictBriefAttribs first
-  #File for filename, may need to trim, used for crafting link
-  strBrief
-  
-  return
-
-  
-def blog_notes():
-  '''
-    
-    strCWD = os.getcwd()
-    strSetOutput = f"0 current directory - [ {strCWD} ]<br>"
-    #strCWD = os.path.dirname(os.path.realpath(__file__))
-    #strSetOutput += f"1 current directory - [ {strCWD} ]<br>"
-    
-    #contents = os.listdir(app_root)
-
-    #for item in contents:
-      #strSetOutput += f"{item}"
-      
-    #file_path = os.path.join(app_root, '_posts', '2025-0925-welcome.md')
-    #strSetOutput += f"3 file path after joins - [ {file_path} ]<br>"
-    
-    #with open(f"{file_path}", "r") as p:
-      #strContent = p.read()
-
-    #strSetOutput += f"4 file content - [ {strContent} ]"
-    
-    os.chdir("_posts") #avoid... chdir impacts actions going forward and on other pages
-    strSetOutput += "1 chdir worked<br>"
-    strCWD = os.getcwd()
-    strSetOutput += f"2 current directory - [ {strCWD} ]<br>"
-
-    #lstFiles = filter(os.path.isfile, os.listdir())
-    strSetOutput += f"3 filter worked<br>"
-    
-    strSetOutput += f"4 files listing worked [ {lstFiles} ]<br><br>"
-
-    strSetOutput += f"5 path join worked [ {lstSortedFiles} ]<br><br>"
-    strSetOutput += "6 sort worked<br>"
-    strSetOutput += f"7 sort worked [ {lstSortedFiles} ]<br><br>"
-  
-  '''
-  return
