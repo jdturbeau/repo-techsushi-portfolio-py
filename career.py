4c75f20defbbb4b36a8560e3d393a24fa8dbc11d
JD Turbeau <turbeaucroteau@gmail.com>
2025-10-09 17:24:09 -0400

    Create career.py

---
+++ career.py
@@ -0,0 +1,19 @@
+# display list of questions to determine job/position suggestions in IT
+'''
+
+manager
+project manager
+architect
+network
+server / platform / cloud admin
+support
+red team / hacker
+blue team / defense
+documentation
+identity and access management
+data manager / sql dev / ETL
+software app backend (code)
+software app front end
+quality testing
+
+'''
